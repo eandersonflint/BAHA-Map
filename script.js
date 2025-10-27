14698f76efbe69f38cedb4d4b0f9f907fdcf22a3
eandersonflint <eandersonflint@berkeley.edu>
2025-10-27 16:52:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWFuZGVyc29uZmxpbnQiLCJhIjoiY21oOXJjdTQzMWdkZjJrcHVxZ3UyMjBydiJ9.xFXiH4wZO0Bi0PCx898FfA';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dpbG1vdHQiLCJhIjoiY2s2bWRjb2tiMG1xMjNqcDZkbGNjcjVraiJ9.2nNOYL23A1cfZSE4hdC9ew';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/eandersonflint/cmh9rgfvw00qk01sm58wqg51i', // your Style URL goes here
+  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9 // starting zoom
+    });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3dpbG1vdHQiLCJhIjoiY2s2bWRjb2tiMG1xMjNqcDZkbGNjcjVraiJ9.2nNOYL23A1cfZSE4hdC9ew';
